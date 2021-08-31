5cfe7f92e3b64d34ec47df6e1398297bc07887f5
Richard Jean-Baptiste <Richinbk1@gmail.com>
2021-08-30 21:45:27 -0400

    lol didnt finish the ui almost tho :)

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
-import { React } from 'react';
+import { React, useState } from 'react';
 import logo from './images/my_unsplash_logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ImageCard from './components/ImageCard';
+import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
+import Form from 'react-bootstrap/Form'
 import beach from './images/beach.jpg';
 import bee from './images/bee.jpg';
 import dev from './images/dev.jpg';
@@ -11,32 +14,110 @@ import path from './images/path.jpg';
 
 function App(props) {
 
+  const [show, setShow] = useState(false);
 
-  
+  const appStyle = () => {
 
-  /*
-  const [ intro, setIntro ] = useState("");
+    let showAlert = "";
+    if(show === false){
+      showAlert = 1;
+    }else{
+      showAlert = 0.3;
+    }
+    
+    let opac = {
+      opacity: showAlert,
+    }
 
-  fetch('/hello')
-  .then(response => response.text())
-  .then(data => setIntro(data))
-  */
+    return opac;
+  }
+
+  let alertStyle = {
+    position: 'absolute',
+    width: '40%',
+    height: '60%',
+    opacity: 1,
+    borderRadius: '12px',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    zIndex: '1',
+    backgroundColor: 'white',
+  }
+
+  const alertHeading = {
+    position: 'absolute',
+    left: '5%',
+    top: '10%',
+  }
+
+  const alertButtons = {
+    position: 'absolute',
+    bottom: '4%',
+    right: '7%',
+  }
+
+  const formStyle = {
+    position: 'absolute',
+    top: '30%',
+  }
+
+  const formInputStyle = {
+    width: '30.5em',
+    borderRadius: '12px',
+  }
+
+  /**
+   *    TODO
+   * 
+   *     Form start icon
+   *     Fonts
+   *     
+   */
 
   return (
     <div className="App">
-        <div className='container'>
+        <Alert show={show} variant="default" style={alertStyle}>
+              <Alert.Heading style={alertHeading}>Add a new photo</Alert.Heading>
+
+              <Form style={formStyle}>
+                <Form.Group>
+                  <div>
+                      <Form.Label style={{marginLeft: '-90%'}}>Label</Form.Label>
+                      <Form.Control style={formInputStyle} type="text" placeholder="Suspendisse elit massa"/>
+                  </div>
+                  <div style={{marginTop: '5%'}}>
+                      <Form.Label style={{marginLeft: '-83%'}}>Photo URL</Form.Label>
+                      <Form.Control  style={formInputStyle} type="text" placeholder="https://images.unsplash.com/photo-1584395630827-860eee694d7b?ixlib=r..."/>
+                  </div>
+                  
+                </Form.Group>
+              </Form>
+              
+              
+              <div className="d-flex justify-content-end" style={alertButtons}>
+                <Button onClick={() => setShow(false)} variant="light" style={{marginRight: '11%', backgroundColor: 'transparent'}}>
+                    Cancel
+                </Button>
+                <Button onClick={() => setShow(false)} variant="success" style={{borderRadius: '12px'}}>
+                    Submit
+                </Button>
+              </div>
+            </Alert>
+
+        <div className='container' style={appStyle()}>
             <div className="AppBar">
                 <img  className="AppLogo" src={logo} alt='unsplash logo' width='14%' height='9%'/>
                 <input className="SearchBar" type="text" placeholder='Search by name'/>
-                <button className="AddButton">Add a photo</button>
+                <button className="AddButton" onClick={() => setShow(true)}>Add a photo</button>
             </div>
+
             <div className="imageContainer">
 
                 <div className="imageColumn">
                     <ImageCard ImageUrl={beach} imgWidth='18em' imgHeight='14em'/>
                     <ImageCard ImageUrl={bee} imgWidth='18em' imgHeight='14em'/>
-                    <ImageCard ImageUrl={dev} imgWidth='18em' imgHeight='14em'/>
-                    
+                    <ImageCard ImageUrl={dev} imgWidth='18em' imgHeight='14em'/>   
                 </div>
 
                 <div className="imageColumn" style={{marginLeft: '5%'}}>
--- client/src/components/ImageCard.js
+++ client/src/components/ImageCard.js
@@ -1,24 +1,82 @@
-import { React } from 'react';
-import Card from 'react-bootstrap/Card'
+import { React, useState } from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 
 function ImageCard(props) {
 
-    
-    const imageStyle1 = {
+    const [showOverlay, setShowOverlay] = useState(false)
+
+    const [imageStyle] = useState({
+        marginBottom: '7%',
         width: props.imgWidth,
         height: props.imgHeight,
         borderRadius: '16px',
         backgroundImage: `url(${props.ImageUrl})`,
         backgroundPosition: 'center center',
         backgroundSize: 'cover',
-        marginBottom: '7%',
-      }
+    })
+
+    const [buttonStyle] = useState({
+        fontFamily: 'Montserrat',
+        fontSize: '15px',
+        borderRadius: '38px',
+        width: '5em',
+        height: '2em',
+        position: 'absolute',
+        right: 30,
+    })
+
+    const cardText = {
+        fontFamily: 'Montserrat',
+        fontSize: '15px',
+        lineHeight: '22px',
+        fontWeight: 'bold',
+        fontStlye: 'normal',
+        color: '#FFFFFF',
+        position: 'absolute',
+        bottom: 40,
+        width: '250px',
+    }
+
+    const overlayStyle = () => {
+
+        let displayState = "";
+
+        if(showOverlay === false){
+            displayState = 'none'
+        }else{
+            displayState = 'block'
+        }
+
+        const show = {
+            position: 'relative',
+            display: displayState,
+            width: props.imgWidth,
+            height: props.imgHeight,
+        }
+
+        
+
+        return show;
+    }
+    
 
     return (
-        <Card style={imageStyle1}>
-            
+        
+        <Card style={imageStyle} onMouseOver={() => setShowOverlay(true)} onMouseLeave={() => setShowOverlay(false)}>
+            <Card.ImgOverlay>
+                <div style={overlayStyle()} >
+                    <Button style={buttonStyle} variant="outline-danger">delete</Button>
+                    <Card.Text style={cardText}>
+                        Morbi consequat lectus non orci maximus
+                    </Card.Text>
+                </div>
+            </Card.ImgOverlay>
         </Card>
+            
+        
+        
     )
 }
 
